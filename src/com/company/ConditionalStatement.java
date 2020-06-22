93f4b8d137b6b83b408aff82e3a6de393b17685e
lindseybarnes <barnesl23@Greenhill.org>
2020-06-22 11:22:52 -0500

    hello

--- src/com/company/ConditionalStatement.java
+++ src/com/company/ConditionalStatement.java
@@ -21,36 +21,38 @@ Create another else if statement that prints Not Weird if n > 20
 Finish by adding brackets to close the code
 */
 package com.company;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.Random; //this allows us to use Random
+import java.util.Scanner; //this allows us to use Scanner
 public class ConditionalStatement {
     public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
-        Random rand = new Random();
-        String answer;
-        int n;
-        do {
+        Scanner keyboard = new Scanner(System.in); //naming scanner
+        Random rand = new Random(); ///naming random
+        String answer; //creating string variable
+        int n; //creating integer
+        do { //tells the computer to display another random integer and decide weather its weird or not if person wants to continue
             n = rand.nextInt(137) + 1;
             System.out.println(n);
-            System.out.println("Do you want to continue?");
+            solve(n); //calling method
+            System.out.println("Do you want to continue (yes or no?)"); //asks user if they want to continue
             answer = keyboard.next();
         }
-        while (answer.contains("Y") || answer.contains("y"));
-        solve (n);
+        while (answer.contains("Y") || answer.contains("y")); //program continues if answer with Y or y is entered
     }
-    public static void solve(int n) {
-        if (n % 2 == 1) {
+
+    public static void solve(int n) { //receiving method
+        if (n % 2 == 1) { //if the remainder is one then it is odd
             System.out.println("Weird");
-        } else if (n <= 6) {
+        } else if (n <= 6) { //if the number is six or under
             System.out.println("Not Weird");
-        } else if (n <= 20) {
+        } else if (n <= 20) { //if the number is from 7 to 20
             System.out.println("Weird");
-        } else if (n > 20) {
+        } else if (n > 20) { //if the number is over twenty
             System.out.println("Not Weird");
         }
-
     }
 }
+
+
 
 
 
